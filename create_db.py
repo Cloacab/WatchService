773ef40296dc8e7c3379206759b01b640399686a
Dronicho <takemyenergy55@gmail.com>
2020-04-17 20:15:06 +0500

    fix jobs

--- create_db.py
+++ create_db.py
@@ -4,6 +4,7 @@ import os.path
 from migrate.versioning import api
 
 from app import db, api as app_api, app
+from app.api import get_covid, get_currencies
 
 SQLALCHEMY_DATABASE_URI = app.config['SQLALCHEMY_DATABASE_URI']
 SQLALCHEMY_MIGRATE_REPO = app.config['SQLALCHEMY_MIGRATE_REPO']
@@ -21,9 +22,9 @@ def bootstrap(scheduler):
         print('Something happened')
 
     try:
-        scheduler.add_job(app_api.Covid().get_info, 'interval', id='covid_job',
+        scheduler.add_job(get_covid, 'interval', id='covid_job',
                           minutes=app.config['REQUEST_INTERVAL'])
-        scheduler.add_job(app_api.Currencies().get_info, 'interval', id='currencies_job',
+        scheduler.add_job(get_currencies, 'interval', id='currencies_job',
                           minutes=app.config['REQUEST_INTERVAL'])
     except Exception as e:
         print('Jobs already created')
